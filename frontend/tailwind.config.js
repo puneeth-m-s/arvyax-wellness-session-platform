ad81889def1f743ca6a69dc61819f0300e1c7d65
puneeth-m-s <pes2ug23cs817@pesu.pes.edu>
2025-07-29 21:45:15 +0530

    29/07

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"], // Or ./src/**/*.{html,js} if your files are in /src
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
